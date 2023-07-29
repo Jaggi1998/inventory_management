3e1c683237675e47cec66859ac4fd08355d6081a
jaggy1998 <jagjots28@gmail.com>
2023-07-29 14:44:54 +0530

    newCode

--- frontend/src/redux/features/product/productService.js
+++ frontend/src/redux/features/product/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACKEND_URL = "http://localhost:5000";
+const BACKEND_URL = "http://localhost:6000";
 
 const API_URL = `${BACKEND_URL}/api/products/`;
 
--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { toast } from "react-toastify";
 
-export const BACKEND_URL = "http://localhost:5000";
+export const BACKEND_URL = "http://localhost:6000";
 
 export const validateEmail = (email) => {
   return email.match(
